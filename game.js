5a34876a28d377a7e95a89cf2c7e7e3710e768bc
mrwhit05 <mrwhit05@att.net>
2026-07-26 15:07:44 -0500

    Added all possible win conditions

--- game.js
+++ game.js
@@ -4,8 +4,65 @@ const ctx = canvas.getContext("2d");
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-function update() {
+let grid = ["", "", "",
+            "", "", "",
+            "", "", ""];
 
+let gameOver = false;
+
+function update() {
+    //win con A top row
+    if (grid[0] != "" && grid[0] === grid[1] && grid[1] === grid[2]){
+        console.log("win A");
+        gameOver = true;
+    }
+
+    //win con B mid row
+    if (grid[3] != "" && grid[3] === grid[4] && grid[4] === grid[5]){
+        console.log("win B");
+        gameOver = true;
+    }
+
+    //win con C bottom row
+    if (grid[6] != "" && grid[6] === grid[7] && grid[7] === grid[8]){
+        console.log("win C");
+        gameOver = true;
+    }
+
+    //win con D left col
+    if (grid[0] != "" && grid[0] === grid[3] && grid[3] === grid[6]){
+        console.log("win D");
+        gameOver = true;
+    }
+
+    //win con E mid col
+    if (grid[1] != "" && grid[1] === grid[4] && grid[4] === grid[7]){
+        console.log("win E");
+        gameOver = true;
+    }
+
+    //win con F right col
+    if (grid[2] != "" && grid[2] === grid[5] && grid[5] === grid[8]){
+        console.log("win F");
+        gameOver = true;
+    }
+
+    //win con G L > R diag
+    if (grid[0] != "" && grid[0] === grid[4] && grid[4] === grid[8]){
+        console.log("win G");
+        gameOver = true;
+    }
+
+    //win con H R > L diag
+    if (grid[2] != "" && grid[2] === grid[4] && grid[4] === grid[6]){
+        console.log("win H");
+        gameOver = true;
+    }
+
+    //tie con
+    //turn count = 10 
+    //console.log("tie");
+    //gameOver = true;
 }
 
 function draw() {
@@ -18,6 +75,11 @@ function draw() {
 function gameLoop() {
     update();
     draw();
+
+    if (gameOver) {
+        return;
+    }
+
     requestAnimationFrame(gameLoop);
 }
 
